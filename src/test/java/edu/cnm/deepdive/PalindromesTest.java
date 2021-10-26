ea5646e1f42d25f24519e6ea60ec9ad14c3d1135
zcr003 <zramsey1@cnm.edu>
2021-10-26 14:08:20 -0600

    Began Palindrome test.

--- src/test/java/edu/cnm/deepdive/PalindromesTest.java
+++ src/test/java/edu/cnm/deepdive/PalindromesTest.java
@@ -3,15 +3,24 @@ package edu.cnm.deepdive;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromesTest {
-  static final String[] args {
-    "radar"	true
-    "sonar"	false
-    "abba"	true
-    "abb"	false
-    "x"	true
-    ""	true
 
+  static final String[] args = {
+    "radar",
+    "sonar",
+    "abba",
+    "abb",
+    "x",
+    "",
 
-  }
+  };
 
+  static final boolean[] expected = {
+      true,
+      false,
+      	true,
+      	false,
+      	true,
+      	true,
+
+  };
 }
